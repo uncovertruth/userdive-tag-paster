3b9c2c91a290bec63193ffcb3b08cda5de406271
taka-sho <takasho530000@gmail.com>
2016-08-03 19:12:36 +0900

    fix eslint error

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,9 +1,10 @@
-(function(){
-  var ele = document.getElementById("status");
-  ele.append("<h1>Yet</h1>");
+(function () {
+  const ele = document.getElementById('status');
+  ele.append('<h1>Yet</h1>');
   chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.greeting == "hello")
-      ele.append("<h1>successed</h1>");
+  function (request, sender, sendResponse) {
+    if (request.greeting === 'hello') {
+      ele.append('<h1>successed</h1>');
+    }
   });
 })();
