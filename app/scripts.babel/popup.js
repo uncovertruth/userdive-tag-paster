d70d5d9bd1bb1f5dec84cbbac4ae0eb13ec87abc
taka-sho <takasho530000@gmail.com>
2016-08-03 18:51:27 +0900

    Add popup.js

---
+++ app/scripts.babel/popup.js
@@ -0,0 +1,9 @@
+'use strict';
+
+(function () {
+  const ele = document.getElementById('status');
+  ele.append('<h1>Yet</h1>');
+  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.greeting === 'hello') ele.append('<h1>successed</h1>');
+  });
+})();
