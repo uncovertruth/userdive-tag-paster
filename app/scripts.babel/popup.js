b05426fa608c72369076669e6b641d89722d5f28
taka-sho <takasho530000@gmail.com>
2016-08-16 17:20:33 +0900

    popup windowに情報を表示

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,7 +1,20 @@
 (function () {
-  const ele = document.getElementById('status');
-  chrome.runtime.sendMessage({greeting: 'ok'}, function (res) {
-    console.log(res.nowStatus);
-    ele.innerText = res.nowStatus;
+  // const ele = document.getElementById('status');
+  // chrome.runtime.sendMessage({greeting: 'ok'}, function (res) {
+  //   console.log(res.sta);
+  //   ele.innerText = res.nowStatus;
+  // });
+  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {greeting: 'get'}, function (response) {
+      // document.getElementById('status').textContent = response.status;
+      // console.log(response.status);
+      const data = response.status.split(',');
+      const ele = document.getElementById('status');
+      for (const i in data) {
+        const z = document.createElement('p');
+        z.innerHTML = data[i];
+        ele.appendChild(z);
+      }
+    });
   });
 })();
