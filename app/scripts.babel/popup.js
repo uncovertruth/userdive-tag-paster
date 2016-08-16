6a9c299094ea0cc68bb27677b65f61c707a7c1cd
taka-sho <takasho530000@gmail.com>
2016-08-16 17:47:21 +0900

    不要な行削除

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,8 +1,6 @@
 (function () {
   chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {greeting: 'get'}, function (response) {
-      // document.getElementById('status').textContent = response.status;
-      // console.log(response.status);
       const data = response.status.split(',');
       const th = document.getElementsByTagName('tr')[1];
       for (const i in data) {
