6c2775be4b549b99e4fc355f610e1e464c47720d
Keisuke Kan <kan@uncovertruth.jp>
2016-08-18 09:24:22 +0900

    refactor(app/scripts.babel) refactor scoped functions

--- app/scripts.babel/tag.js
+++ app/scripts.babel/tag.js
@@ -5,7 +5,7 @@
     element.setAttribute('${attr}', 'used');
   } else {
     /* eslint-disable */
-    (function(e,t,n,c,r,a,s,u){e.USERDIVEObject=r;e[r]=e[r]||function(){(e[r].queue=e[r].queue||[]).push(arguments)};s=t.createElement(n);u=t.getElementsByTagName(n)[0];s.async=1;s.src=c;s.charset=a;u.parentNode.insertBefore(s,u)})(window,document,"script","//harpoon3.userdive.com/static/UDTracker.js?"+(new Date).getTime(),"ud","UTF-8");
+    (function(e,t,n,c,r,a,s,u){e.USERDIVEObject=r;e[r]=e[r]||function(){(e[r].queue=e[r].queue||[]).push(arguments)};s=t.createElement(n);u=t.getElementsByTagName(n)[0];s.async=1;s.src=c;s.charset=a;u.parentNode.insertBefore(s,u)})(window,document,"script","//${host}/static/UDTracker.js?"+(new Date).getTime(),"ud","UTF-8");
     /* eslint-enable */
     root.ud('create', '${id}', {env: '${env}', cookieExpires: 1});
     root.ud('analyze');
