5a68f18deb46cdf75d7020ed6eb91434be1bd28e
taka-sho <takasho530000@gmail.com>
2016-08-31 18:13:51 +0900

    use hash in popup.js

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -13,28 +13,13 @@
       });
     }
     appendState (status) {
-      const statusAry = this.changeStatusToAry(status);
+      const statusHash = JSON.parse(status);
+      console.log(statusHash);
       const ele = document.getElementsByTagName('info')[0];
-      for (const i in statusAry) {
-        for (let index = 0; index < 2; index++) {
-          if (index === 0) {
-            ele.setAttribute(statusAry[i][index] + 'title', statusAry[i][index]);
-          } else {
-            ele.setAttribute(statusAry[i][0], statusAry[i][index]);
-          }
-        }
+      for (const key in statusHash) {
+        ele.setAttribute(key + 'title', statusHash[key]);
       }
       riot.mount('*');
-    }
-
-    changeStatusToAry (status) {
-      const attrStatus = status.split(',');
-      const data = [];
-      for (const i in attrStatus) {
-        data.push(attrStatus[i]);
-        data.push(attrStatus[i].split(':'));
-      }
-      return data;
     }
     updateState () {
       chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
