baae2975aacb90af702af8bbef2c2545c6e35cb3
Keisuke Kan <kan@uncovertruth.jp>
2016-10-26 21:38:53 +0900

    chore(package) add eslint-plugin-flowtype

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
-  extends: ['@uncovertruth/eslint-config'],
+  extends: [
+    '@uncovertruth/eslint-config',
+    "plugin:flowtype/recommended"
+  ],
+  plugins: [
+    "flowtype"
+  ],
   parser: 'babel-eslint',
   env: {
     'node': true
