c6d11f1dcc8c6b1634a6c6c97b6fd1955a8646f4
Keisuke Kan <kan@uncovertruth.jp>
2016-11-29 15:37:12 +0900

    fix(app/scripts.babel) fix require timing

--- app/scripts.babel/tag.js
+++ app/scripts.babel/tag.js
@@ -6,8 +6,8 @@
     /* eslint-disable */
     (function(e,t,n,c,r,a,s,u){e.USERDIVEObject=r;e[r]=e[r]||function(){(e[r].queue=e[r].queue||[]).push(arguments)};s=t.createElement(n);u=t.getElementsByTagName(n)[0];s.async=1;s.src=c;s.charset=a;u.parentNode.insertBefore(s,u)})(window,document,"script","//${host}/static/UDTracker.js?"+(new Date).getTime(),"ud","UTF-8");
     /* eslint-enable */
-    global[entrypoint]('create', '${id}', {env: '${env}', cookieExpires: 1});
-    global[entrypoint]('analyze');
+    global.ud('create', '${id}', {env: '${env}', cookieExpires: 1});
+    global.ud('analyze');
   }
   setTimeout(function () {
     if (!agent) {
