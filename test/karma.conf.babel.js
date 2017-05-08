6829d83058f359949a23db9d05aa5411999467f9
taka-sho <takasho530000@gmail.com>
2017-05-08 18:43:34 +0900

    chore(karma.conf) fix option

--- test/karma.conf.babel.js
+++ test/karma.conf.babel.js
@@ -43,7 +43,7 @@ export default (config) => {
     }
   };
   if (process.env.TRAVIS) {
-    config.browsers = ['Chrome_travis_ci'];
+    karmaConfig.browsers = ['Chrome_travis_ci'];
   }
   config.set(karmaConfig);
 };
