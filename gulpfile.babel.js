3b19111214cdb718f8e9d83fe5b5e2c8a0c2558f
taka-sho <takasho530000@gmail.com>
2017-06-16 16:55:22 +0900

    chore(gulpfile) remove images task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,29 +5,6 @@ import runSequence from 'run-sequence'
 
 const $ = gulpLoadPlugins()
 
-gulp.task('images', () => {
-  return gulp
-    .src('app/images/**/*')
-    .pipe(
-      $.if(
-        $.if.isFile,
-        $.cache(
-          $.imagemin({
-            progressive: true,
-            interlaced: true,
-            // don't remove IDs from SVGs, they are often used
-            // as hooks for embedding and styling
-            svgoPlugins: [{ cleanupIDs: false }]
-          })
-        ).on('error', function (err) {
-          console.log(err) // eslint-disable-line no-console
-          this.end()
-        })
-      )
-    )
-    .pipe(gulp.dest('dist/images'))
-})
-
 gulp.task('chromeManifest', () => {
   return gulp
     .src('app/manifest.json')
@@ -47,10 +24,23 @@ gulp.task('size', () => {
   return gulp.src('dist/**/*').pipe($.size({ title: 'build', gzip: true }))
 })
 
+gulp.task('package', function () {
+  const manifest = require('./dist/manifest.json')
+  const app = require('./package.json')
+
+  if (app.version !== manifest.version) {
+    throw new Error('Pleas update verions')
+  }
+
+  return gulp
+    .src('dist/**')
+    .pipe($.zip(app.name + '-' + manifest.version + '.zip'))
+    .pipe(gulp.dest('package'))
+})
+
 gulp.task('build', cb => {
   runSequence(
     'chromeManifest',
-    'images',
     'size',
     cb
   )
