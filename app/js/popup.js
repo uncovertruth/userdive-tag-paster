ae1c228e2d802a0f1bdf5a86feaf0e8870bc9b93
Takasho <takasho530000@gmail.com>
2017-08-08 09:11:18 +0900

    refact(js/) remove immediate functions (#137)
    * chore(background) add disable status (#123)
    
    * chore(background) add disable status
    
    * chore(background) disable setting
    
    * fix(background) typo
    
    * chore(popup) implement disable
    
    * update(deps)
    
    * chore(background) rename namespace: changeAppStatus
    
    * chore(background) rename namespace: appStatus
    
    * chore(content) typo
    
    * refactor(content-script)
    
    * chore(popup) typo
    
    * chore(contentscript) send-message as promise
    
    * chore(info.vue) display activation label
    
    * chore(info.vue) popup style
    
    * update(deps)
    
    * chore(contentscript) unuse JSON.stringify
    
    * chore(contentscript) remove console.log
    
    * refact(app/js) remove unnecessary methods (#129)
    
    * chore(background) add disable status
    
    * chore(background) disable setting
    
    * fix(background) typo
    
    * chore(popup) implement disable
    
    * update(deps)
    
    * chore(background) rename namespace: changeAppStatus
    
    * chore(background) rename namespace: appStatus
    
    * chore(content) typo
    
    * refactor(content-script)
    
    * chore(popup) typo
    
    * chore(contentscript) send-message as promise
    
    * chore(info.vue) display activation label
    
    * chore(info.vue) popup style
    
    * update(deps)
    
    * chore(contentscript) unuse JSON.stringify
    
    * chore(contentscript) remove console.log
    
    * chore(js/render) use vue instance
    
    * chore(js/render) use vue props
    
    * refact(js/contentscript) remove unnecessary methods
    
    * refact(js/popup) namespace
    
    * chore(js/content) rename key
    
    * chore(js/background) rename const
    
    * refact(js/popup) rename method
    
    * refactor(app/js) methods and names (#131)
    
    * chore(js/content) rename key
    
    * chore(js/background) rename const
    
    * refact(js/popup) rename method
    
    * chore(popup) show when disable
    
    * fix(js/content) fix bug when no options
    
    * refact(js/) unuse immediate functions

--- app/js/popup.js
+++ app/js/popup.js
@@ -1,42 +1,39 @@
 /* @flow */
-'use strict'
 import componentFactory from './render'
 declare var chrome: any
 
-;(function (global, chrome, document) {
-  class StateView {
-    constructor (): void {
-      global.addEventListener('load', evt => {
-        this.render()
-      })
-    }
-    render (): void {
+class StateView {
+  constructor (): void {
+    window.addEventListener('load', evt => {
+      this.render()
+    })
+  }
+  render (): void {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      chrome.tabs.sendMessage(tabs[0].id, { content: 'fetchCookie' }, response => {
+        if (!response || !response.data) {
+          throw new Error("couldn't recieve page data")
+        }
+        this.show(response.data)
+      }
+      )
+    })
+  }
+  show (userData: object): void {
+    componentFactory(userData, () => {
+      this.toggleExtension()
+    })
+  }
+  toggleExtension (): void {
+    chrome.runtime.sendMessage({ bg: 'toggleExtension' }, response => {
+      const isActive = !!response.isActive
       chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-        chrome.tabs.sendMessage(tabs[0].id, { content: 'fetchCookie' }, response => {
-          if (!response || !response.data) {
-            throw new Error("couldn't recieve page data")
-          }
+        chrome.tabs.sendMessage(tabs[0].id, { content: isActive }, response => {
           this.show(response.data)
         }
         )
       })
-    }
-    show (userData: object): void {
-      componentFactory(userData, () => {
-        this.toggleExtension()
-      })
-    }
-    toggleExtension (): void {
-      chrome.runtime.sendMessage({ bg: 'toggleExtension' }, response => {
-        const isActive = !!response.isActive
-        chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-          chrome.tabs.sendMessage(tabs[0].id, { content: isActive }, response => {
-            this.show(response.data)
-          }
-          )
-        })
-      })
-    }
+    })
   }
-  return new StateView()
-})(window, chrome, document)
+}
+new StateView() // eslint-disable-line no-new
