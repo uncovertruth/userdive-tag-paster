0757f0ef6691a7a126b291a292dc6cbf35bf3343
taka-sho <takasho530000@gmail.com>
2017-08-30 16:21:16 +0900

    chore(__tests__/popup, options) click test

--- __tests__/options.spec.jsx
+++ __tests__/options.spec.jsx
@@ -1,9 +1,9 @@
 /* @flow */
 /** @jsx h */
-/* eslint import/first: 0 */
 import 'jest'
 import { h, render } from 'preact'
 import { random } from 'faker'
+import chrome from 'sinon-chrome'
 
 import Options from '../app/components/options'
 
@@ -12,6 +12,12 @@ describe('options', () => {
 
   beforeEach(() => {
     body.innerHTML = ''
+    global.chrome = chrome
+  })
+
+  afterEach(function () {
+    chrome.flush()
+    delete global.chrome
   })
 
   function renderComponent (data) {
@@ -36,5 +42,18 @@ describe('options', () => {
     renderComponent()
     const th = document.getElementsByTagName('input')[0]
     expect(th.value).toBe('')
+  })
+
+  test('onSave', () => {
+    const configs = {
+      id: random.alphaNumeric(10),
+      env: random.alphaNumeric(10),
+      host: random.alphaNumeric(15),
+      ignore: random.alphaNumeric(10)
+    }
+    renderComponent(configs)
+
+    const button = body.getElementsByTagName('button')[0]
+    button.click()
   })
 })
--- __tests__/popup.spec.jsx
+++ __tests__/popup.spec.jsx
@@ -1,23 +1,39 @@
 /* @flow */
 /** @jsx h */
-/* eslint import/first: 0 */
 import 'jest'
 import { h, render } from 'preact'
+import chrome from 'sinon-chrome'
 
 import Popup from '../app/components/popup'
 
 describe('popup', () => {
   const body: any = document.body
 
+  beforeEach(() => {
+    body.innerHTML = ''
+    global.chrome = chrome
+  })
+
+  afterEach(function () {
+    chrome.flush()
+    delete global.chrome
+  })
+
   function renderComponent (data: Object) {
     render(<Popup {...{ data }} />, body)
   }
 
   test('vaild id', () => {
-    const data = { test: 'test' }
-    renderComponent(data)
+    renderComponent({ test: 'test' })
 
     const th: any = body.getElementsByTagName('th')[0]
     expect(th.innerHTML).toBe('test')
+  })
+
+  test('toggle', () => {
+    renderComponent({ test: 'test' })
+
+    const button = body.getElementsByTagName('button')[0]
+    button.click()
   })
 })
