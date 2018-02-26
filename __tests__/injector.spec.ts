39963518fb26bcb7cf1d46f9f11ecf99658c047e
taka-sho <takasho530000@gmail.com>
2018-02-26 18:00:36 +0900

    chore(__tests__/injector.spec.ts) add description

--- __tests__/injector.spec.ts
+++ __tests__/injector.spec.ts
@@ -10,7 +10,7 @@ describe('injector', () => {
       host: random.alphaNumeric(15),
       ignore: random.alphaNumeric(15)
     }
-    inject(elementId, `a${random.uuid()}`, config)
+    inject(elementId, `a${random.uuid()}`, config) // The attribute key must start with an alphabet.
     expect(document.getElementById(elementId))
   })
 
@@ -20,7 +20,7 @@ describe('injector', () => {
       host: random.alphaNumeric(15),
       ignore: 'about:blank'
     }
-    inject(elementId, `a${random.uuid()}`, config)
+    inject(elementId, `a${random.uuid()}`, config) // L13
     expect(document.getElementById(elementId))
   })
 })
